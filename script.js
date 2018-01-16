0e09ca222995fa2f458a1ca4f444cb3cee049921
Sarah Yu <sarahyu0692@gmail.com>
2018-01-16 14:23:59 -0500

    new design

--- script.js
+++ script.js
@@ -19,4 +19,35 @@ $('document').ready(() => {
 	$('#murakami-img').mouseleave(function() {
 		$(this).attr('src', 'img/murakami.png')
 	})
+
+	$('#piece-peace-img').mouseenter(function() {
+		$(this).attr('src', 'img/piece-peace-hover.png')
+	})
+	$('#piece-peace-img').mouseleave(function() {
+		$(this).attr('src', 'img/piece-peace.png')
+	})
+
+	let muscatLink = $('.muscat-link')
+	let muscatImage = $('.muscat-image')
+
+	muscatLink.hover(
+		function() {
+			muscatImage.addClass('muscat-image-show')
+		},
+		function() {
+			muscatImage.removeClass('muscat-image-show')
+		}
+	)
+
+	let contactOpenClose = $('.contact-open-close')
+	let overlay = $('.overlay')
+	let main = $('.main')
+
+	contactOpenClose.on('click', e => {
+		e.preventDefault()
+
+		// $('body').toggleClass('is-active')
+		overlay.fadeToggle(400)
+		main.toggleClass('hide-main')
+	})
 })
